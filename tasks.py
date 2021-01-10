9d5544aaf18a15fc61f24013e306de99674d7375
Carlo De Pieri <depieri.carlo@gmail.com>
2021-01-10 15:55:01 +0100

    added livereload task

--- tasks.py
+++ tasks.py
@@ -12,6 +12,9 @@ PIPENV = "pipenv run"
 INTL_BIN = "{} sphinx-intl".format(PIPENV)
 BUILD_BIN = "{} sphinx-build".format(PIPENV)
 
+# Disable the pipenv spam about already being inside a venv
+os.environ["PIPENV_VERBOSITY"] = "-1"
+
 
 @task
 def clean(c):
@@ -48,3 +51,42 @@ def build_it_html(c):
 def build_html(c):
     print("\n>> Building english html docs...\n")
     c.run("{} -b html -D language=en {} {}".format(BUILD_BIN, SOURCE_FOLDER, HTML_BUILD_FOLDER))
+
+
+@task(build_it_html, build_html)
+def build(c):
+    pass
+
+
+@task()
+def serve(c):
+    from livereload import Server
+    server = Server()
+    # Watch for changes in rst files; rebuild the html documentation when it happens
+    server.watch('docs/source/*.rst', lambda: build_html(c))
+    # Serve the builded docs. This will autoreload on change!
+    server.serve(root='docs/build/html', port=5500)
+
+
+@task()
+def serve_it(c):
+    from livereload import Server
+    server = Server()
+
+    def _build_it_docs(c):
+        build_gettext(c)
+        cwd = os.getcwd()
+        build_it_docs(c)
+        os.chdir(cwd)
+
+    # Build the docs for the first time
+    _build_it_docs(c)
+    build_it_html(c)
+    print("\n>> Docs built! Serving and watching for changes...\n")
+
+    # Watch for any changes in rst files: rebuild po files when it happens
+    server.watch('docs/source/*.rst', lambda: _build_it_docs(c))
+    # Watch for any changes in po files; rebuild the html documentation when it happens
+    server.watch('docs\source\locales\it\LC_MESSAGES\*.po', lambda: build_it_html(c))
+    # Serve the builded docs. This will autoreload on change!
+    server.serve(root='docs/build/html/it', port=5501)
